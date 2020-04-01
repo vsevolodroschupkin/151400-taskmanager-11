c21c926d5c3d115ad3e3a50431a6fd7fdd23cfc5
User <user@MacBook-Pro-Vsevolod.local>
2020-04-01 22:08:32 +0500

    Поправил ошибки линтера и добавил node_modules в игнор

--- src/main.js
+++ src/main.js
@@ -3,114 +3,114 @@
 const TASK_COUNT = 3;
 
 const createSiteMenuTemplate = () => {
-    return (
-        `<section class="control__btn-wrap">
-        <input
-          type="radio"
-          name="control"
-          id="control__new-task"
-          class="control__input visually-hidden"
-        />
-        <label for="control__new-task" class="control__label control__label--new-task"
-          >+ ADD NEW TASK</label
-        >
-        <input
-          type="radio"
-          name="control"
-          id="control__task"
-          class="control__input visually-hidden"
-          checked
-        />
-        <label for="control__task" class="control__label">TASKS</label>
-        <input
-          type="radio"
-          name="control"
-          id="control__statistic"
-          class="control__input visually-hidden"
-        />
-        <label for="control__statistic" class="control__label"
-          >STATISTICS</label
-        >
-      </section>`
-    );
+  return (
+    `<section class="control__btn-wrap">
+      <input
+        type="radio"
+        name="control"
+        id="control__new-task"
+        class="control__input visually-hidden"
+      />
+      <label for="control__new-task" class="control__label control__label--new-task"
+        >+ ADD NEW TASK</label
+      >
+      <input
+        type="radio"
+        name="control"
+        id="control__task"
+        class="control__input visually-hidden"
+        checked
+      />
+      <label for="control__task" class="control__label">TASKS</label>
+      <input
+        type="radio"
+        name="control"
+        id="control__statistic"
+        class="control__input visually-hidden"
+      />
+      <label for="control__statistic" class="control__label"
+        >STATISTICS</label
+      >
+    </section>`
+  );
 };
 
 const createFiltersTemplate = () => {
-    return (
-        `<section class="main__filter filter container">
-            <input
-            type="radio"
-            id="filter__all"
-            class="filter__input visually-hidden"
-            name="filter"
-            checked
-            />
-            <label for="filter__all" class="filter__label">
-            All <span class="filter__all-count">13</span></label
-            >
-            <input
-            type="radio"
-            id="filter__overdue"
-            class="filter__input visually-hidden"
-            name="filter"
-            disabled
-            />
-            <label for="filter__overdue" class="filter__label"
-            >Overdue <span class="filter__overdue-count">0</span></label
-            >
-            <input
-            type="radio"
-            id="filter__today"
-            class="filter__input visually-hidden"
-            name="filter"
-            disabled
-            />
-            <label for="filter__today" class="filter__label"
-            >Today <span class="filter__today-count">0</span></label
-            >
-            <input
-            type="radio"
-            id="filter__favorites"
-            class="filter__input visually-hidden"
-            name="filter"
-            />
-            <label for="filter__favorites" class="filter__label"
-            >Favorites <span class="filter__favorites-count">1</span></label
-            >
-            <input
-            type="radio"
-            id="filter__repeating"
-            class="filter__input visually-hidden"
-            name="filter"
-            />
-            <label for="filter__repeating" class="filter__label"
-            >Repeating <span class="filter__repeating-count">1</span></label
-            >
-            <input
-            type="radio"
-            id="filter__archive"
-            class="filter__input visually-hidden"
-            name="filter"
-            />
-            <label for="filter__archive" class="filter__label"
-            >Archive <span class="filter__archive-count">115</span></label
-            >
-      </section>`
-    );
+  return (
+    `<section class="main__filter filter container">
+          <input
+          type="radio"
+          id="filter__all"
+          class="filter__input visually-hidden"
+          name="filter"
+          checked
+          />
+          <label for="filter__all" class="filter__label">
+          All <span class="filter__all-count">13</span></label
+          >
+          <input
+          type="radio"
+          id="filter__overdue"
+          class="filter__input visually-hidden"
+          name="filter"
+          disabled
+          />
+          <label for="filter__overdue" class="filter__label"
+          >Overdue <span class="filter__overdue-count">0</span></label
+          >
+          <input
+          type="radio"
+          id="filter__today"
+          class="filter__input visually-hidden"
+          name="filter"
+          disabled
+          />
+          <label for="filter__today" class="filter__label"
+          >Today <span class="filter__today-count">0</span></label
+          >
+          <input
+          type="radio"
+          id="filter__favorites"
+          class="filter__input visually-hidden"
+          name="filter"
+          />
+          <label for="filter__favorites" class="filter__label"
+          >Favorites <span class="filter__favorites-count">1</span></label
+          >
+          <input
+          type="radio"
+          id="filter__repeating"
+          class="filter__input visually-hidden"
+          name="filter"
+          />
+          <label for="filter__repeating" class="filter__label"
+          >Repeating <span class="filter__repeating-count">1</span></label
+          >
+          <input
+          type="radio"
+          id="filter__archive"
+          class="filter__input visually-hidden"
+          name="filter"
+          />
+          <label for="filter__archive" class="filter__label"
+          >Archive <span class="filter__archive-count">115</span></label
+          >
+    </section>`
+  );
 };
 
 const createBoardTemplate = () => {
-    return (
-        `<section class="board container">
-            <div class="board__filter-list">
-            <a href="#" class="board__filter" data-sort-type="default">SORT BY DEFAULT</a>
-            <a href="#" class="board__filter" data-sort-type="date-up">SORT BY DATE up</a>
-            <a href="#" class="board__filter" data-sort-type="date-down">SORT BY DATE down</a>
-            </div>
+  return (
+    `<section class="board container">
+      <div class="board__filter-list">
+      <a href="#" class="board__filter" data-sort-type="default">SORT BY DEFAULT</a>
+      <a href="#" class="board__filter" data-sort-type="date-up">SORT BY DATE up</a>
+      <a href="#" class="board__filter" data-sort-type="date-down">SORT BY DATE down</a>
+      </div>
 
-            <div class="board__tasks"></div>
-        </section>`
-    );
+      <div class="board__tasks"></div>
+    </section>`
+  );
 };
 
 const createTaskEditTemplate = () => {
@@ -370,7 +370,7 @@ const createLoadMoreButtonTemplate = () => {
 };
 
 const render = (container, template, place) => {
-    container.insertAdjacentHTML(place, template);
+  container.insertAdjacentHTML(place, template);
 };
 
 const siteMainElement = document.querySelector(`.main`);
@@ -384,7 +384,6 @@ const tasksListElement = siteMainElement.querySelector(`.board__tasks`);
 render(tasksListElement, createTaskEditTemplate(), `beforeEnd`);
 for (let i = 0; i < TASK_COUNT; i++) {
   render(tasksListElement, createTaskTemplate(), `beforeend`);
-};
-
+}
 const boardElement = siteMainElement.querySelector(`.board`);
 render(boardElement, createLoadMoreButtonTemplate(), `beforeend`);
